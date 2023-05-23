7450bbf8ecfee83d5eaa648e619a49936b44e3b8
ryc9092 <ryc9092@gmail.com>
2023-05-23 10:22:09 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # exposeNgrokTunnels
-Expose ngrok tunnels info of device by email when tunnels has changed
+Get ngrok tunnels info of device by email when tunnels has changed
+
+### Description
+This program will send ngrok tunnels info by email when tunnels has changed (maybe device reboot)
+
+### Pre-requirements
+- Ngrok
+Use `~/.config/ngrok/ngrok.yml` to create multiple tunnels by command: `ngrok start --all --config '~/.config/ngrok/ngrok.yml'`, and make it auto start (by service/crontab...).
+
+### Usage
+1. Get google app password for send email
+2. Edit sender, receiver, password in emailConfiguration.yaml
+3. Put the job to crontab: `*/5 * * * * cd /path/to/ExposeNgrokTunnels && python3 /path/to/ExposeNgrokTunnels/main.py`
---
+++ main.py
@@ -0,0 +1,89 @@
+import logging
+import requests
+import yaml
+import smtplib
+import ssl
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+log_handler = logging.FileHandler("/var/log/exposeNgrokTunnels.log")
+log_handler.setLevel(logging.INFO)
+log_handler.setFormatter(formatter)
+
+logger.addHandler(log_handler)
+
+DEVICE_TUNNELS_FILE = "/tmp/exposeNgrokTunnels.yaml"
+EMAIL_CONCONFIG_FILE = "./emailConfiguration.yaml"
+NGROK_API_URL = "http://localhost:4040/api/tunnels"
+
+
+def get_ngrok_tunnels_list() -> list:
+    try:
+        response_ngrok_tunnels_info = requests.get(NGROK_API_URL)
+        ngrok_tunnels_info = response_ngrok_tunnels_info.json().get("tunnels")
+
+        ngrok_tunnels_list = []
+        for tunnel in ngrok_tunnels_info:
+            ngrok_tunnels_list.append(tunnel.get("public_url"))
+
+        ngrok_tunnels_list.sort()
+        return ngrok_tunnels_list
+
+    except Exception as e:
+        logger.error(f"Failed to get ngrok tunnels list: {e}")
+        return []
+
+
+def get_device_tunnels_list() -> list:
+    try:
+        device_tunnels_list = []
+        with open(DEVICE_TUNNELS_FILE, mode="r") as f:
+            device_tunnels_list = yaml.load(f, Loader=yaml.FullLoader).get("tunnels")
+
+    except Exception as e:
+        logger.warning(f"Failed to read device tunnels information: {e}")
+
+    device_tunnels_list.sort()
+    return device_tunnels_list
+
+
+def write_tunnels_info(tunnels_list: list):
+    logger.info(f"Write tunnels info to device: {tunnels_list}")
+    with open(DEVICE_TUNNELS_FILE, "w") as f:
+        yaml.dump({"tunnels": tunnels_list}, f)
+
+
+def read_email_configuration() -> dict:
+    with open(EMAIL_CONCONFIG_FILE, "r") as f:
+        email_config = yaml.load(f, Loader=yaml.FullLoader)
+    return email_config
+
+
+def send_tunnels_info_email(ngrok_tunnels: list):
+    try:
+        email_config = read_email_configuration()
+        content = f"""Subject: Ngrok tunnels info of un42\n\n{ngrok_tunnels}"""
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(email_config["smtp_server"], context=context) as server:
+            server.login(email_config["sender"], email_config["password"])
+            server.sendmail(email_config["sender"], email_config["receiver"], content)
+
+    except Exception as e:
+        logger.error(f"Failed to send ngrok tunnels info email: {e}")
+
+
+if __name__ == "__main__":
+    try:
+        ngrok_tunnels_list = get_ngrok_tunnels_list()
+        device_tunnels_list = get_device_tunnels_list()
+
+        if (not device_tunnels_list) or (ngrok_tunnels_list != device_tunnels_list):
+            write_tunnels_info(ngrok_tunnels_list)
+            send_tunnels_info_email(ngrok_tunnels_list)
+
+    except Exception as e:
+        logger.error(f"Failed to expose tunnels: {e}")
